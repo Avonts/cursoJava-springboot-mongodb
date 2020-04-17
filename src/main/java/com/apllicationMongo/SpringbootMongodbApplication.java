17061db01ebff21973b965efdc9b3f01928e4b08
Avonts <vinialves022@gmail.com>
2020-04-16 23:43:38 -0300

    Adicionando o UserResource

--- src/main/java/com/apllicationMongo/SpringbootMongodbApplication.java
+++ src/main/java/com/apllicationMongo/SpringbootMongodbApplication.java
@@ -1,4 +1,4 @@
-package com.viniciusalves.apllicationMongo;
+package com.apllicationMongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/apllicationMongo/resources/UserResource.java
@@ -0,0 +1,29 @@
+package com.apllicationMongo.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.apllicationMongo.domain.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<User>> findAll() {
+
+		User Maria = new User("1", "Maria Brow", "maria.gmail.com");
+		User Alex = new User("2", "Alex Green", "alex.gmail.com");
+
+		List<User> list = new ArrayList<>();
+		list.addAll(Arrays.asList(Maria, Alex));
+		return ResponseEntity.ok().body(list);
+	}
+
+}
