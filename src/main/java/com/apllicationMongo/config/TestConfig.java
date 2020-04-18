b5529e2ea81824a7821f992019f745d6f1b0b8ec
Avonts <vinialves022@gmail.com>
2020-04-18 16:48:04 -0300

    Adicionando o AuthorDTO

--- src/main/java/com/apllicationMongo/config/TestConfig.java
+++ src/main/java/com/apllicationMongo/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.apllicationMongo.domain.Post;
 import com.apllicationMongo.domain.User;
+import com.apllicationMongo.dto.AuthorDTO;
 import com.apllicationMongo.repository.PostRepository;
 import com.apllicationMongo.repository.UserRepository;
 
@@ -21,13 +22,13 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private PostRepository postRepository;
-	
+
 	@Override
 	public void run(String... args) throws Exception {
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-		
+
 		userRepository.deleteAll();
 		postRepository.deleteAll();
 
@@ -35,10 +36,11 @@ public class TestConfig implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-		Post post1 = new Post(null,sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar pra são paulo, abraços!", maria);
-		Post post2 = new Post(null,sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar pra são paulo, abraços!",	new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
