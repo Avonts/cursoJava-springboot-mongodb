d82c90c199bee8296c4910ee3949700337825ba2
Avonts <vinialves022@gmail.com>
2020-04-18 18:23:46 -0300

    Adicionando comentários aos posts

--- src/main/java/com/apllicationMongo/config/TestConfig.java
+++ src/main/java/com/apllicationMongo/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.apllicationMongo.domain.Post;
 import com.apllicationMongo.domain.User;
 import com.apllicationMongo.dto.AuthorDTO;
+import com.apllicationMongo.dto.CommentDTO;
 import com.apllicationMongo.repository.PostRepository;
 import com.apllicationMongo.repository.UserRepository;
 
@@ -41,6 +42,13 @@ public class TestConfig implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar pra são paulo, abraços!",
 				new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 
 		postRepository.saveAll(Arrays.asList(post1, post2));
 
