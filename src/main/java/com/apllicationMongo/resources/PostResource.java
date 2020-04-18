9701e9d7b1841bb99f0c334b41989b594d0329ff
Avonts <vinialves022@gmail.com>
2020-04-18 18:08:10 -0300

    Adicionando o endPoint findById

---
+++ src/main/java/com/apllicationMongo/resources/PostResource.java
@@ -0,0 +1,25 @@
+package com.apllicationMongo.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.apllicationMongo.domain.Post;
+import com.apllicationMongo.services.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+	@Autowired
+	PostService service;
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Post obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}	
+}
